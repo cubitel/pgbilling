5738d41bb7c56e79b9dd8c92cc4a4ab1184ebe15
Vasily Voropaev <vvg@cubitel.org>
2016-12-11 15:00:54 +0300

    sql: Modified test data

--- sql/test-data.sql
+++ sql/test-data.sql
@@ -6,7 +6,9 @@ DECLARE
     account_id integer;
     service_id integer;
 BEGIN
-    INSERT INTO users (user_name, login_type, login, pass) VALUES('Иванов С. С.', 1, 'siva', '123');
+	INSERT INTO operators (operator_name, login_type, login, pass) VALUES('Администратор', 1, 'admin', sha1('pgbilling'));
+
+    INSERT INTO users (user_name, login_type, login, pass) VALUES('Абонент', 1, 'test', '123');
     SELECT lastval() INTO user_id;
 
     INSERT INTO accounts (user_id, account_number, balance) VALUES(user_id, '1000011', 10);
@@ -15,10 +17,7 @@ BEGIN
     INSERT INTO services (user_id, account_id, service_type, service_name, service_pass, inet_speed) VALUES(user_id, account_id, 1, 'TEST-01', '123', 10000);
     SELECT lastval() INTO service_id;
 
-    INSERT INTO services_addr (service_id, ip_address) VALUES(service_id, '1.2.3.4');
-    INSERT INTO services_addr (service_id, ip_address) VALUES(service_id, '2a00:d48::/32');
-
-    INSERT INTO services (user_id, account_id, service_type, service_name, service_state) VALUES(user_id, account_id, 1, 'TEST-02', 2);
+    INSERT INTO services_addr (service_id, ip_address) VALUES(service_id, '192.168.0.10');
 
     INSERT INTO radius_attrs (service_state, attr_name, attr_value) VALUES(1, 'Dynamic-Qos-Param', 'police-circuit-rate rate-absolute {kbps}');
     INSERT INTO radius_attrs (service_state, attr_name, attr_value) VALUES(1, 'Dynamic-Qos-Param', 'meter-circuit-rate rate-absolute {kbps}');
