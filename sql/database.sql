808a94f5939c0c0d7e77a5e072a9dca59d553880
Vasily Voropaev <vvg@cubitel.org>
2019-11-10 14:33:55 +0300

    sql: Drop schema 'billing'

--- sql/database.sql
+++ sql/database.sql
@@ -2,7 +2,6 @@ BEGIN TRANSACTION;
 
 \i def-system.sql
 \i def-admin.sql
-\i def-billing.sql
 \i def-cabinet.sql
 \i def-network.sql
 \i def-payments.sql
--- sql/drop-all.sql
+++ sql/drop-all.sql
@@ -1,9 +1,9 @@
 
-DROP SCHEMA IF EXISTS system, admin, billing, cabinet, network, site CASCADE;
+DROP SCHEMA IF EXISTS system, admin, cabinet, network, payments, site CASCADE;
 
 CREATE SCHEMA system;
 CREATE SCHEMA admin;
-CREATE SCHEMA billing;
 CREATE SCHEMA cabinet;
 CREATE SCHEMA network;
+CREATE SCHEMA payments;
 CREATE SCHEMA site;
